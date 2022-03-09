7dbbaaa4851c433276424844a539dace80e76f1a
Sivakumaran E <sivakumaran.e@gmail.com>
2022-03-09 11:20:28 +0530

    save data to db

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
-const database = [];
+const database = new Datastore("database.db");
+database.loadDatabase();
+// database.insert({ name: "siva", status: ':rainbow' });
+// database.insert({ name: "sam", status: ':happy' });
+
 
 app.post("/api", (request, response) => {
   console.log(request.body);
   const data = request.body;
-  database.push(data);
-  console.log(database);
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
   response.json({
     status: "success.!",
+    timestamp: timestamp,
     latitude: data.lat,
     longitude: data.lon,
   });
 });
+ ///TODO:
+ /***** 
+ 09/03/22
+ Add one more field in the UI and save it to the database
+ *******/
